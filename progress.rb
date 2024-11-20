03761d05b84b50eee10e3d04fa79c93653d60a90
Maria Hernandez <herna835@csusm.edu>
2024-11-19 19:07:36 -0800

    first commit

---
+++ progress.rb
@@ -0,0 +1,9 @@
+
+#This file is going to keep track of chapters have been read
+
+{
+    "lastReadDay" : 1,
+    "lastReadBook": Genesis,
+    "lastReadChapter" : 1
+
+}
---
+++ schedule.rb
@@ -0,0 +1,6 @@
+# This file is going to divide the BIBLE into 365 sections
+
+{
+    "1": {"book": "Genesis", "chapter": 1}
+    
+}
